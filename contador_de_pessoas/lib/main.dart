6c5bc47cd819f67145d3b9da3ee8e9cb37b64239
Railson Martins <railson170@hotmail.com>
2020-09-16 20:56:12 -0300

    Colunas e textos

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -3,5 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contados de pessoas",
-      home: Column(children: <Widget>[Text("Pessoas:  0")])));
+      home: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas:  0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "pode Entrar!!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ])));
 }
