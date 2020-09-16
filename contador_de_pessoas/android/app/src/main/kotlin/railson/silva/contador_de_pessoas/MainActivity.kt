f494c27c9054b0eef033a6d71bda5bc80f4dc827
Railson Martins <railson170@hotmail.com>
2020-09-16 20:50:45 -0300

    start do projeeto

---
+++ contador_de_pessoas/android/app/src/main/kotlin/railson/silva/contador_de_pessoas/MainActivity.kt
@@ -0,0 +1,6 @@
+package railson.silva.contador_de_pessoas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ contador_de_pessoas/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Contados de pessoas",
+      home: Column(children: <Widget>[Text("Pessoas:  0")])));
+}
