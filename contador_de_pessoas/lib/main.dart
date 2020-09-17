974ce23de8e470866d04d5dba3633d2f53bb6d17
Railson Martins <railson170@hotmail.com>
2020-09-16 21:59:20 -0300

    Statefull, Stateless

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -1,56 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Contados de pessoas",
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/restaurant.jpg",
-            fit: BoxFit.cover,
-            height: 1000,
+  runApp(MaterialApp(title: "Contados de pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = "Pode Entrar!";
+
+  void _changePeoble(int delta) {
+    setState(() {
+      _people += delta;
+      if (_people < 0) {
+        _infoText = "Putz";
+      } else if (_people <= 10) {
+        _infoText = "Pode Entrar!";
+      } else {
+        _infoText = "Lotado";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000,
+        ),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Text(
+            "Pessoas:  $_people",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
-          Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Pessoas:  0",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                  child: Text(
+                    "+1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {
+                    _changePeoble(1);
+                  },
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: FlatButton(
-                        child: Text(
-                          "+1",
-                          style: TextStyle(fontSize: 40.0, color: Colors.white),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: FlatButton(
-                        child: Text(
-                          "-1",
-                          style: TextStyle(fontSize: 40.0, color: Colors.white),
-                        ),
-                        onPressed: () {},
-                      ),
-                    )
-                  ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                  child: Text(
+                    "-1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {
+                    _changePeoble(-1);
+                  },
                 ),
-                Text(
-                  "Pode Entrar!!",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 30.0),
-                )
-              ])
-        ],
-      )));
+              )
+            ],
+          ),
+          Text(
+            "$_infoText",
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ])
+      ],
+    );
+  }
 }
