fa3beacc43a8fb54b87294eacb8594f937343947
Railson Martins <railson170@hotmail.com>
2020-09-16 21:06:40 -0300

    Row

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -11,6 +11,25 @@ void main() {
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  child: Text(
+                    "+1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  child: Text(
+                    "-1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
             Text(
               "pode Entrar!!",
               style: TextStyle(
